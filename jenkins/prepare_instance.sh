13ad03b7a2d3919291522433bb669e3794f788d5
Daniel Santillan <daniel.santillan@eox.at>
2013-12-10 13:38:51 +0100

    Test of pip option to disable extended reftools

--- jenkins/prepare_instance.sh
+++ jenkins/prepare_instance.sh
@@ -44,7 +44,7 @@ source .venv/bin/activate
 
 # Uninstall/Install EOxServer
 pip uninstall --yes EOxServer
-pip install --upgrade git+https://github.com/v-manip/eoxserver.git –install-option="--disable-extended-reftools"
+pip install -–install-option="--disable-extended-reftools" --upgrade git+https://github.com/v-manip/eoxserver.git 
 
 # Install ngEO Browse Server
 echo "**> installing ngeo-b..."
