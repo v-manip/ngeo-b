8a466a7a6a4f6a334bc1a2fc6f306992dd4a37fe
Martin Hecher <martin.hecher@fraunhofer.at>
2014-03-14 15:51:37 +0100

    Added creation of Cloudsat sqlite-db at provision time

--- vagrant/scripts/user_custom.sh
+++ vagrant/scripts/user_custom.sh
@@ -13,3 +13,21 @@ EOF
 # Create gltf folder, which is then exported via httpd
 mkdir /var/www/cache/gltf
 chown vagrant.vagrant /var/www/cache/gltf
+
+# Create Cloudsat sqlite.db
+cat << EOF > /tmp/bootstrap_cloudsat.txt
+create table if not exists tiles(
+                        tileset text,
+                        grid text,
+                        x integer,
+                        y integer,
+                        z integer,
+                        data text,
+                        dim text,
+                        ctime datetime,
+                        primary key(tileset,grid,x,y,z,dim)
+                    );
+
+EOF
+sqlite3 /var/www/cache/Cloudsat.sqlite < /tmp/bootstrap_cloudsat.txt
+rm /tmp/bootstrap_cloudsat.txt
