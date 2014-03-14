222443f89fdb4695c5d2a8683882b2434ed74a6b
Martin Hecher <martin.hecher@fraunhofer.at>
2014-03-14 15:28:47 +0100

    Added creation of 'gltf' folder at provision time

--- vagrant/scripts/user_custom.sh
+++ vagrant/scripts/user_custom.sh
@@ -9,3 +9,7 @@ EOF
 cat << EOF >> /home/vagrant/.bashrc
 alias l="ls -lah"
 EOF
+
+# Create gltf folder, which is then exported via httpd
+mkdir /var/www/cache/gltf
+chown vagrant.vagrant /var/www/cache/gltf
