5c39d36d119f4f0eef65aa452499f0cfc927aec7
Daniel Santillan <daniel.santillan@eox.at>
2014-03-18 17:19:34 +0100

    share /var/lib was creating problems when creating vagrant mashine, changed to /var/vmanip/lib as well as /var/data to /var/vmanip/data for consistency

--- vagrant/scripts/load_test_data.sh
+++ vagrant/scripts/load_test_data.sh
@@ -2,12 +2,12 @@
 
 cd /var/ngeob_autotest
 
-python manage.py loaddata /var/data/cloudsat_new/cloudsat_fixture.json
+python manage.py loaddata /var/vmanip/data/cloudsat_new/cloudsat_fixture.json
 
 tmpdir=`mktemp -d`
 
 #find /var/data/cloudsat/ -name 'cloudsat_2014012*' -exec unzip {} -d ${tmpdir} \;
-find /var/data/cloudsat/ -name 'cloudsat_20140124150000_37519.zip' -exec unzip {} -d ${tmpdir} \;
+find /var/vmanip/data/cloudsat/ -name 'cloudsat_20140124150000_37519.zip' -exec unzip {} -d ${tmpdir} \;
 
 find ${tmpdir} -name '*tif' -exec mv {} /var/www/store/ \;
 
